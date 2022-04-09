1fbc52254909a06317cffca582cb78ac4c30388e
Tomasz Woźniak <wozniak.tom@pm.me>
2022-04-10 00:37:12 +1000

    beautiful utils

---
+++ src/utils.cpp
@@ -0,0 +1,22 @@
+
+#include <RcppArmadillo.h>
+#include "Rcpp/Rmath.h"
+
+using namespace Rcpp;
+using namespace arma;
+
+
+/*______________________function orthogonal_complement_matrix_TW______________________*/
+mat orthogonal_complement_matrix_TW (const mat& x) {
+  // # x is a mxn matrix and m>n
+  // # the function returns a mx(m-n) matrix, out, that is an orthogonal complement of x, i.e.:
+  // # t(x)%*%out = 0 and det(cbind(x,out))!=0
+  int n_nrow     = x.n_rows;
+  int n_ncol     = x.n_cols;
+  mat Q;
+  mat R;
+  qr(Q, R, x);
+  mat ocm = Q.tail_cols(n_nrow-n_ncol);
+  return ocm;
+} // END orthogonal_complement_matrix_TW
+
