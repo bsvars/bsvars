435c0c659d8fcfc1b53c39832d31ac38bf40d5e9
Tomasz Woźniak <wozniak.tom@pm.me>
2022-04-10 23:48:05 +1000

    Added help to normalisation_wz2003

--- src/normalisation.cpp
+++ src/normalisation.cpp
@@ -32,7 +32,34 @@ arma::rowvec normalisation_wz2003_s (
 
 
 
-/*______________________function normalisation_wz2003______________________*/
+//' @title Waggoner & Zha (2003) row signs normalisation of the posterior draws for matrix \code{B}
+//'
+//' @description Normalises the sign of rows of matrix \code{B} MCMC draws, 
+//'  provided as the first argument \code{posterior_B}, relative to matrix
+//'  \code{B_hat}, provided as the second argument of the function. The implemented
+//'  procedure proposed by Waggoner, Zha (2003) normalises the MCMC output in an
+//'  optimal way leading to the unimodal posterior. Only normalised MCMC output is 
+//'  suitable for the computations of the posterior characteristics of the \code{B}
+//'  matrix elements and their functions such as the impulse response functions and other 
+//'  economically interpretable values. 
+//' 
+//' @param posterior_B an \code{NxNxS} array containing \code{S} draws from the posterior 
+//' distribution of the \code{NxN} matrix of contemporaneous relationships \code{B}. 
+//' These draws are to be normalised with respect to:
+//' @param B_hat an \code{NxN} matrix specified by the user to have the desired row signs
+//' 
+//' @return Nothing. The normalised elements overwrite the corresponding elements of 
+//' the first argument \code{posterior_B} by reference.
+//' 
+//' @seealso [bsvar()]
+//'
+//' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
+//' 
+//' @references 
+//' Waggoner, D.F., and Zha, T., (2003) Likelihood Preserving Normalization in Multiple Equation Models. 
+//' \emph{Journal of Econometrics}, \bold{114}(2), 329--47, \doi{https://doi.org/10.1016/S0304-4076(03)00087-3}.
+//'
+//' @export
 // [[Rcpp::export]]
 void normalisation_wz2003 (
     arma::cube& posterior_B,            // NxNxS
