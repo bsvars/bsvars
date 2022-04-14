fe978381cd53874dee2d7b6e9d57315db4a14042
Tomasz Woźniak <wozniak.tom@pm.me>
2022-04-15 01:05:31 +1000

    Created an R wrapper file with documentation
    for normalisation and logSDDR

---
+++ R/logSDDR_homoskedasticity.R
@@ -0,0 +1,44 @@
+
+#' @title The log of Bayes factor for the homoskedasticity hypothesis for each structural shock
+#'
+#' @description Computes the logarithm of Bayes factor for the homoskedasticity hypothesis 
+#' for each of the structural shocks via Savage-Dickey Density Ration (SDDR).
+#' The hypothesis is represented by restriction:
+#' \Sexpr[results=rd, stage=build]{katex::math_to_rd("w_n = 0")}
+#' The logarithm of Bayes factor for this hypothesis can be computed using the SDDR 
+#' as the difference of logarithms of the marginal posterior distribution ordinate at the restriction 
+#' less the marginal prior distribution ordinate at the same point:
+#' \Sexpr[results=rd, stage=build]{katex::math_to_rd("log p(w_n = 0 | data) - log p(w_n = 0)")}
+#' Therefore, a negative value of the difference is the evidence against 
+#' homoskedasticity of the structural shock. The estimation of both elements of the difference requires 
+#' numerical integration.
+#' 
+#' @param posterior the \code{posterior} element of the list from the estimation outcome obtained using function \code{bsvar_sv}
+#' @param prior A list specifying the prior distribution. See argument \code{prior} of function \code{bsvar_sv}
+#' @param Y an \code{NxT} matrix, the matrix containing \code{T} observations on \code{N} dependent time series variables
+#' @param X a \code{KxT} matrix, the matrix containing \code{T} observations on \code{K = N*p+d} regressors including \code{p} lags of dependent variables and \code{d} deterministic terms
+#' @param sample_s_ a logical value set to the same value as the corresponding argument of function \code{bsvar_sv}
+#' 
+#' @return A list of two components:
+#' 
+#' \code{logSDDR} an \code{N}-vector with values of the logarithm of the Bayes factors for 
+#' the homoskedasticity hypothesis for each of the shocks
+#' 
+#' \code{components} a list of three components for the computation of the Bayes factor
+#' \describe{
+#'   \item{log_denominator}{an \code{N}-vector with values of the logarithm of the Bayes factor denominators}
+#'   \item{log_numerator}{an \code{N}-vector with values of the logarithm of the Bayes factor numerators}
+#'   \item{log_numerator_s}{an \code{NxS} matrix of the log-full conditional posterior density ordinates computed to estimate the numerator}
+#' }
+#' 
+#' @seealso \code{\link{bsvar_sv}}
+#'
+#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
+#' 
+#' @references 
+#' Lütkepohl, H., Shang, F., Uzeda, L., and Woźniak, T. (2022) Partial Identification of Heteroskedastic Structural VARs: Theory and Bayesian Inference.
+#'
+#' @export
+logSDDR_homoskedasticity <- function(posterior, prior, Y, X, sample_s_ = TRUE) {
+  logSDDR_homoskedasticity(posterior, prior, Y, X, sample_s_)
+}
---
+++ R/normalisation_wz2003.R
@@ -0,0 +1,34 @@
+
+
+#' @title Waggoner & Zha (2003) row signs normalisation of the posterior draws for matrix \code{B}
+#'
+#' @description Normalises the sign of rows of matrix \code{B} MCMC draws, 
+#'  provided as the first argument \code{posterior_B}, relative to matrix
+#'  \code{B_hat}, provided as the second argument of the function. The implemented
+#'  procedure proposed by Waggoner, Zha (2003) normalises the MCMC output in an
+#'  optimal way leading to the unimodal posterior. Only normalised MCMC output is 
+#'  suitable for the computations of the posterior characteristics of the \code{B}
+#'  matrix elements and their functions such as the impulse response functions and other 
+#'  economically interpretable values. 
+#' 
+#' @param posterior_B an \code{NxNxS} array containing \code{S} draws from the posterior 
+#' distribution of the \code{NxN} matrix of contemporaneous relationships \code{B}. 
+#' These draws are to be normalised with respect to:
+#' @param B_hat an \code{NxN} matrix specified by the user to have the desired row signs
+#' 
+#' @return Nothing. The normalised elements overwrite the corresponding elements of 
+#' the first argument \code{posterior_B} by reference.
+#' 
+#' @seealso \code{\link{bsvar}}, \code{\link{bsvar_msh}}, \code{\link{bsvar_sv}}, \code{\link{bsvar_ihmm}}
+#' \code{\link{bsvar_mix}}, \code{\link{bsvar_dph}}
+#'
+#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
+#' 
+#' @references 
+#' Waggoner, D.F., and Zha, T., (2003) Likelihood Preserving Normalization in Multiple Equation Models. 
+#' \emph{Journal of Econometrics}, \bold{114}(2), 329--47, \doi{https://doi.org/10.1016/S0304-4076(03)00087-3}.
+#'
+#' @export
+normalisation_wz2003 <- function(posterior_B, B_hat) {
+  invisible(.Call(`_bsvars_normalisation_wz2003`, posterior_B, B_hat))
+}
