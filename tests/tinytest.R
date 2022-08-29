7e0df0054dc141a5b47403d10c0853069046c43a
Tomasz Woźniak <wozniak.tom@pm.me>
2022-08-29 23:07:58 +1000

    Corrected Suggests in DESCRIPTION
    and it gives 000 at CHECK!

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,4 @@
-
-if ( requireNamespace("tinytest", quietly=TRUE) ) {
+if ( requireNamespace("tinytest", quietly = TRUE) ) {
   # To skip running the tests on CRAN make them run only on the developer version 
   home <- length(unclass(packageVersion("bsvars"))[[1]]) == 4
   tinytest::test_package("bsvars", at_home = home)
