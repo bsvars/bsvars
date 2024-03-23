ec0077cd683639a9cc4289bcd842c6ab9b1a6040
Tomasz Woźniak <wozniak.tom@pm.me>
2024-03-23 14:49:54 +1100

    method summary for PosteriorIR #1

--- R/summary.R
+++ R/summary.R
@@ -233,3 +233,85 @@ summary.PosteriorHD = function(
   
   return(out)
 } # END summary.PosteriorHD
+
+
+
+
+
+
+
+#' @title Provides posterior summary of variables' fitted values
+#'
+#' @description Provides posterior summary of the fitted values including their 
+#' mean, standard deviations, as well as 5 and 95 percentiles.
+#' 
+#' @param object an object of class PosteriorIR obtained using the
+#' \code{compute_impulse_responses()} function containing draws the predictive 
+#' density of the sample data. 
+#' @param ... additional arguments affecting the summary produced.
+#' 
+#' @return A list reporting the posterior mean, standard deviations, as well as 
+#' 5 and 95 percentiles of the impulse responses of each variable to each of the 
+#' shocks at all horizons.
+#' 
+#' @method summary PosteriorIR
+#' 
+#' @seealso \code{\link{compute_impulse_responses}}
+#'
+#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
+#' 
+#' @examples
+#' # upload data
+#' data(us_fiscal_lsuw)
+#' 
+#' # specify the model and set seed
+#' set.seed(123)
+#' specification  = specify_bsvar$new(us_fiscal_lsuw, p = 1)
+#' 
+#' # run the burn-in
+#' burn_in        = estimate(specification, 10)
+#' 
+#' # estimate the model
+#' posterior      = estimate(burn_in$get_last_draw(), 20, , thin = 1)
+#' 
+#' # compute impulse responses
+#' irf            = compute_impulse_responses(posterior, horizon = 8)
+#' irf_summary    = summary(irf)
+#' 
+#' # workflow with the pipe |>
+#' ############################################################
+#' set.seed(123)
+#' us_fiscal_lsuw |>
+#'   specify_bsvar$new(p = 1) |>
+#'   estimate(S = 10) |> 
+#'   estimate(S = 20, thin = 1) |> 
+#'   compute_impulse_responses(horizon = 8) |>
+#'   summary() -> irf_summary
+#' 
+#' @export
+summary.PosteriorIR = function(
+    object,
+    ...
+) {
+  
+  N         = dim(object)[1]
+  H         = dim(object)[3] - 1
+  
+  out       = list()
+  for (n in 1:N) {
+    out[[n]] = list()
+    for (i in 1:N) {
+      out[[n]][[i]]    = cbind(
+        apply(object[i,n,,], 1, mean),
+        apply(object[i,n,,], 1, sd),
+        t(apply(object[i,n,,], 1, quantile, probs = c(0.05, 0.95)))
+      )
+      colnames(out[[n]][[i]]) = c("mean", "sd", "5% quantile", "95% quantile")
+      rownames(out[[n]][[i]]) = 0:H
+    } # END i loop
+    names(out[[n]]) = paste0("variable", 1:N)
+  } # END n loop
+  names(out) = paste0("shock", 1:N)
+  
+  return(out)
+} # END summary.PosteriorIR
