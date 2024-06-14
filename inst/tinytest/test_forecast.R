7c6d120ca2398162f50c7031ef0cb7eca972bdfe
Tomasz Woźniak <wozniak.tom@pm.me>
2024-06-15 00:35:11 +1000

    included tests of conditional forecasting #76

--- inst/tinytest/test_forecast.R
+++ inst/tinytest/test_forecast.R
@@ -216,3 +216,139 @@ expect_identical(
   info = "forecast: sv centred: sigma forecast identical for normal and pipe workflow."
 )
 
+
+
+
+
+# conditional forecasting
+################################
+cf        = matrix(NA , 2, 3)
+cf[,3]    = tail(us_fiscal_lsuw, 1)[3]   # conditional forecasts equal to the last gdp observation
+
+# for bsvar
+set.seed(1)
+suppressMessages(
+  specification_no1 <- specify_bsvar$new(us_fiscal_lsuw)
+)
+run_no1             <- estimate(specification_no1, 3, 1, show_progress = FALSE)
+ff                  <- forecast(run_no1, horizon = 2, conditional_forecast = cf)
+
+set.seed(1)
+suppressMessages(
+  ff2              <- us_fiscal_lsuw |>
+    specify_bsvar$new() |>
+    estimate(S = 3, thin = 1, show_progress = FALSE) |>
+    forecast(horizon = 2, conditional_forecast = cf)
+)
+
+
+expect_identical(
+  ff$forecasts[1,1,1], ff2$forecasts[1,1,1],
+  info = "conditonal forecast: forecast identical for normal and pipe workflow."
+)
+
+expect_true(
+  is.numeric(ff$forecasts) & is.array(ff$forecasts),
+  info = "conditonal forecast: returns numeric array."
+)
+
+expect_error(
+  forecast(run_no1, horizon = 3, conditional_forecast = cf),
+  info = "conditonal forecast: wrong value of horizon."
+)
+
+
+# for bsvar_msh
+set.seed(1)
+suppressMessages(
+  specification_no1 <- specify_bsvar_msh$new(us_fiscal_lsuw, M = 2)
+)
+run_no1             <- estimate(specification_no1, 3, 1, show_progress = FALSE)
+ff                  <- forecast(run_no1, horizon = 2, conditional_forecast = cf)
+
+set.seed(1)
+suppressMessages(
+  ff2              <- us_fiscal_lsuw |>
+    specify_bsvar_msh$new(M = 2) |>
+    estimate(S = 3, thin = 1, show_progress = FALSE) |>
+    forecast(horizon = 2, conditional_forecast = cf)
+)
+
+
+expect_identical(
+  ff$forecasts[1,1,1], ff2$forecasts[1,1,1],
+  info = "conditonal forecast: msh: forecast identical for normal and pipe workflow."
+)
+
+expect_identical(
+  ff$forecasts_sigma[1,1,1], ff2$forecasts_sigma[1,1,1],
+  info = "conditonal forecast: msh: sigma forecast identical for normal and pipe workflow."
+)
+
+
+
+
+# for bsvar_mix
+set.seed(1)
+suppressMessages(
+  specification_no1 <- specify_bsvar_mix$new(us_fiscal_lsuw, M = 2)
+)
+run_no1             <- estimate(specification_no1, 3, 1, show_progress = FALSE)
+ff                  <- forecast(run_no1, horizon = 2, conditional_forecast = cf)
+
+set.seed(1)
+suppressMessages(
+  ff2              <- us_fiscal_lsuw |>
+    specify_bsvar_mix$new(M = 2) |>
+    estimate(S = 3, thin = 1, show_progress = FALSE) |>
+    forecast(horizon = 2, conditional_forecast = cf)
+)
+
+expect_identical(
+  ff$forecasts[1,1,1], ff2$forecasts[1,1,1],
+  info = "conditonal forecast: mix: forecast identical for normal and pipe workflow."
+)
+
+expect_identical(
+  ff$forecasts_sigma[1,1,1], ff2$forecasts_sigma[1,1,1],
+  info = "conditonal forecast: mix: sigma forecast identical for normal and pipe workflow."
+)
+
+expect_true(
+  is.numeric(ff$forecasts) & is.array(ff$forecasts),
+  info = "conditonal forecast: mix: returns numeric array."
+)
+
+
+
+
+
+
+
+# for bsvar_sv
+set.seed(1)
+suppressMessages(
+  specification_no1 <- specify_bsvar_sv$new(us_fiscal_lsuw)
+)
+run_no1             <- estimate(specification_no1, 3, 1, show_progress = FALSE)
+ff                  <- forecast(run_no1, horizon = 2, conditional_forecast = cf)
+
+set.seed(1)
+suppressMessages(
+  ff2              <- us_fiscal_lsuw |>
+    specify_bsvar_sv$new() |>
+    estimate(S = 3, thin = 1, show_progress = FALSE) |>
+    forecast(horizon = 2, conditional_forecast = cf)
+)
+
+
+expect_identical(
+  ff$forecasts[1,1,1], ff2$forecasts[1,1,1],
+  info = "conditonal forecast: sv: forecast identical for normal and pipe workflow."
+)
+
+expect_identical(
+  ff$forecasts_sigma[1,1,1], ff2$forecasts_sigma[1,1,1],
+  info = "conditonal forecast: sv: sigma forecast identical for normal and pipe workflow."
+)
+
