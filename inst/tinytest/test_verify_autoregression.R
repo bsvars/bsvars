43d0c4b4e6d78d8ca387a4b6fc2e7d90576af975
Tomasz Woźniak <wozniak.tom@pm.me>
2025-04-09 23:24:57 +1000

    tests for exclusion restrictions for A #106

--- inst/tinytest/test_verify_autoregression.R
+++ inst/tinytest/test_verify_autoregression.R
@@ -35,3 +35,21 @@ expect_error(
   pattern = "*numeric*",
   info = "verify_autoregressive: H0 is not numeric"
 )
+
+# models with exclusion restrictions on autoregressive terms
+A = matrix(TRUE, 3, 4)
+A[1,3] = FALSE
+
+set.seed(1)
+suppressMessages(
+  specification_no2 <- specify_bsvar$new(us_fiscal_lsuw, A = A)
+)
+run_no2             <- estimate(specification_no2, 60, 1, show_progress = FALSE)
+
+H0             = matrix(NA, ncol(us_fiscal_lsuw), ncol(us_fiscal_lsuw) + 1)
+H0[1,3]        = 0
+expect_error(
+  verify_autoregression(run_no2, H0),
+  pattern = "restricted",
+  info = "verify_autoregressive: H0 verifies autoregressive restrictions"
+)
